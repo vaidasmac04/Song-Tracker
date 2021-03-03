6cbd856f5d6cd95c88153119400b7516ba867cc1
Vaidas <vaidasmac04@gmail.com>
2021-03-03 12:56:24 +0200

    H2 database connection added & Singer entity annotated as database table

--- src/main/java/lt/vu/entities/Singer.java
+++ src/main/java/lt/vu/entities/Singer.java
@@ -3,12 +3,25 @@ package lt.vu.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Getter
 @Setter
 public class Singer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String firstName;
     private String lastName;
     private String stageName;
+
+    public Singer(){
+
+    }
 
     public Singer(String firstName, String lastName, String stageName) {
         this.firstName = firstName;
