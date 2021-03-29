f950f0682acbf5adb35d1d7ca1731402b2783db4
Vaidas <vaidasmac04@gmail.com>
2021-03-30 00:19:36 +0300

    Song page structure changed & song adding form modified to submit more information

---
+++ src/main/java/lt/vu/usecases/Genres.java
@@ -0,0 +1,33 @@
+package lt.vu.usecases;
+
+import lombok.Getter;
+import lombok.Setter;
+import lt.vu.entities.Genre;
+import lt.vu.entities.Song;
+import lt.vu.persistence.GenresDAO;
+import lt.vu.persistence.MusiciansDAO;
+import lt.vu.persistence.SongsDAO;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Model
+public class Genres {
+    @Inject
+    private GenresDAO genresDAO;
+
+    @Getter
+    private List<Genre> allGenres;
+
+    @PostConstruct
+    public void init(){
+        loadAllGenres();
+    }
+
+    private void loadAllGenres(){
+        allGenres = genresDAO.loadAll();
+    }
+}
