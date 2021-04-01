0087271597181c6f41846555f3f8cb0bd13bcca9
Vaidas <vaidasmac04@gmail.com>
2021-04-01 14:01:37 +0300

    Layout changes in multiple pages

--- src/main/java/lt/vu/usecases/SingleAlbum.java
+++ src/main/java/lt/vu/usecases/SingleAlbum.java
@@ -2,8 +2,10 @@ package lt.vu.usecases;
 
 import lombok.Getter;
 import lombok.Setter;
+import lt.vu.entities.Album;
 import lt.vu.entities.Musician;
 import lt.vu.entities.Song;
+import lt.vu.persistence.AlbumsDAO;
 import lt.vu.persistence.MusiciansDAO;
 import lt.vu.persistence.SongsDAO;
 
@@ -11,37 +13,27 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Model
-public class SongsOfMusician {
+public class SingleAlbum {
     @Inject
-    private MusiciansDAO musiciansDAO;
+    private AlbumsDAO albumsDAO;
 
     @Inject
     private SongsDAO songsDAO;
 
     @Getter
     @Setter
-    private Musician musician;
-
-    @Getter
-    @Setter
-    private Song songToCreate = new Song();
+    private Album album;
 
     @PostConstruct
     public void init() {
         Map<String, String> requestParameters =
                 FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-        Integer musicianId = Integer.parseInt(requestParameters.get("musicianId"));
-        this.musician = musiciansDAO.findOne(musicianId);
-    }
-
-    @Transactional
-    public String createSong() {
-        songToCreate.setMusician(this.musician);
-        songsDAO.persist(songToCreate);
-        return "/musician-songs?faces-redirect=true&musicianId=" + this.musician.getId();
+        Integer albumId = Integer.parseInt(requestParameters.get("albumId"));
+        this.album = albumsDAO.findOne(albumId);
     }
 }
--- src/main/java/lt/vu/usecases/SingleMusician.java
+++ src/main/java/lt/vu/usecases/SingleMusician.java
@@ -2,10 +2,8 @@ package lt.vu.usecases;
 
 import lombok.Getter;
 import lombok.Setter;
-import lt.vu.entities.Album;
 import lt.vu.entities.Musician;
 import lt.vu.entities.Song;
-import lt.vu.persistence.AlbumsDAO;
 import lt.vu.persistence.MusiciansDAO;
 import lt.vu.persistence.SongsDAO;
 
@@ -13,32 +11,22 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 @Model
-public class SongsOfAlbum {
+public class SingleMusician {
     @Inject
-    private AlbumsDAO albumsDAO;
-
-    @Inject
-    private SongsDAO songsDAO;
+    private MusiciansDAO musiciansDAO;
 
     @Getter
     @Setter
-    private Album album;
-
-    @Getter
-    @Setter
-    private List<Song> allAlbumSongs = new ArrayList();
+    private Musician musician;
 
     @PostConstruct
     public void init() {
         Map<String, String> requestParameters =
                 FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-        Integer albumId = Integer.parseInt(requestParameters.get("albumId"));
-        this.album = albumsDAO.findOne(albumId);
-        this.allAlbumSongs = songsDAO.findByAlbum(albumId);
+        Integer musicianId = Integer.parseInt(requestParameters.get("musicianId"));
+        this.musician = musiciansDAO.findOne(musicianId);
     }
 }
